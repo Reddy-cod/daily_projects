ee30d6307d936115a77886a3f4c064661fbaf62e
Thummala Surendra Reddy <surendrareddyt660@gmail.com>
2025-04-30 10:15:40 +0530

    Enumeration

---
+++ april30/src/main/java/com/ibm/april30/App.java
@@ -0,0 +1,13 @@
+package com.ibm.april30;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
