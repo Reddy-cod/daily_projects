5dd3a61fa34f2af5495f3cbcb5a95b8d8bb92139
Thummala Surendra Reddy <surendrareddyt660@gmail.com>
2025-05-02 10:54:09 +0530

    Generic Exception handler

--- Exceptions/src/main/java/com/ibm/Exceptions/Application.java
+++ Exceptions/src/main/java/com/ibm/Exceptions/Application.java
@@ -1,19 +1,21 @@
 package com.ibm.Exceptions;
 
+import java.util.Scanner;
+
 public class Application {
           public static void main(String[] args) {
 			System.out.println("Start");
+			Scanner s=new Scanner(System.in);
 			try {
 				System.out.println(args[0]);
+				int a =s.nextInt();
+				int b=s.nextInt();
 			} 
-			catch (ArrayIndexOutOfBoundsException e) {
+			catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
 				System.out.println("Run the program supplying a number");
 			}
 			catch (ArithmeticException e) {
 				System.out.println("You cannot divide by zero");
-			}
-			catch (NumberFormatException  e) {
-				System.out.println("Provide valid numbers");
 			}catch(Exception e) {
 				System.out.println("Unknown error");
 			}
