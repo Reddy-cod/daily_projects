8f1c29b24b25d804d60fb7ec4e5a5566cec20915
Thummala Surendra Reddy <surendrareddyt660@gmail.com>
2025-05-12 17:01:15 +0530

    reactts interfaces

--- React/react-typescript/src/App.tsx
+++ React/react-typescript/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+interface GreetingsProp{
+  name: string;
+  city: string;
+}
+function Greetings({name, city}:GreetingsProp) {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Hello {name} from {city}</h2>
+    </>
+  )
+}
+function App() {
+  return (
+    <>
+     <Greetings name="IBMer" city ="bangalore"/>
     </>
   )
 }
